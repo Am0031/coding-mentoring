2fea1ecc7b82ea7bf59e2c599aac3ad151389cb8
CSim <100964977+C-Sim@users.noreply.github.com>
2022-07-07 20:23:46 +0100

    Mentee model draft

---
+++ src/models/Mentee.js
@@ -0,0 +1,133 @@
+const { Model, DataTypes } = require("sequelize");
+const bcrypt = require("bcrypt");
+
+const sequelize = require("../config/connection");
+
+const { hashPassword } = require("../hooks");
+
+class Mentee extends Model {
+  getUser() {
+    return {
+      id: this.id,
+      firstName: this.firstName,
+      lastName: this.lastName,
+      userName: this.userName,
+      email: this.email,
+      location: this.location,
+      availability: this.availability,
+      learningFormat: this.learningFormat,
+      personalGoal: this.personalGoal,
+      profileImageUrl: this.profileImageUrl,
+      gitHubUrl: this.gitHubUrl,
+      linkedInUrl: this.linkedInUrl,
+      portfolioUrl: this.portfolioUrl,
+    };
+  }
+
+  async checkPassword(password) {
+    const isValid = await bcrypt.compare(password, this.password);
+    return isValid;
+  }
+}
+
+Mentee.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    firstName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [2, 50],
+      },
+    },
+    lastName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [2, 50],
+      },
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [2, 50],
+      },
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isEmail: true,
+      },
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [2, 50],
+      },
+    },
+    location: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    availability: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    learningFormat: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    personalGoal: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    profileImageUrl: {
+      type: DataTypes.TEXT,
+      allowNull: true,
+      validate: {
+        isUrl: true,
+      },
+    },
+    gitHubUrl: {
+      type: DataTypes.TEXT,
+      allowNull: true,
+      validate: {
+        isUrl: true,
+      },
+    },
+    linkedInUrl: {
+      type: DataTypes.TEXT,
+      allowNull: true,
+      validate: {
+        isUrl: true,
+      },
+    },
+    portfolioUrl: {
+      type: DataTypes.TEXT,
+      allowNull: true,
+      validate: {
+        isUrl: true,
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "mentee",
+    hooks: {
+      beforeCreate: hashPassword,
+    },
+  }
+);
+
+module.exports = Mentee;
