222d8e5049aff1072f152875629f457363eba038
Amelie Pira <amelie.pira@gmail.com>
2022-07-08 11:56:34 +0100

    amended "routes" word in main index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const path = require("path");
 const express = require("express");
 
 const connection = require("./config/connection");
-const router = require("./routes/index");
+const routes = require("./routes");
 
 const PORT = process.env.PORT || 4000;
 
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "../public")));
-app.use(router);
+app.use(routes);
 
 const init = async () => {
   try {
