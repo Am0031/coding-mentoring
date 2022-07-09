34c0e5a8d56f03d7f5bd041d6bbea37ec69bad0f
Amelie Pira <amelie.pira@gmail.com>
2022-07-09 14:19:04 +0100

    added getMentors controller (with filter)

--- src/routes/api/mentees.js
+++ src/routes/api/mentees.js
@@ -5,14 +5,12 @@ const router = Router();
 const {
   getAllMentees,
   getMenteeById,
-  createMentee,
   updateMenteeById,
   deleteMenteeById,
 } = require("../../controllers/api/mentees");
 
 router.get("/", getAllMentees);
 router.get("/:id", getMenteeById);
-router.post("/", createMentee);
 router.put("/:id", updateMenteeById);
 router.delete("/:id", deleteMenteeById);
 
