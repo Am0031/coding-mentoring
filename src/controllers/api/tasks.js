3d93f4937e086b82e0f8ae2422bc88b87ab371b1
Amelie Pira <amelie.pira@gmail.com>
2022-07-11 09:58:04 +0100

    added update task function

--- src/controllers/api/tasks.js
+++ src/controllers/api/tasks.js
@@ -127,8 +127,27 @@ const createTask = async (req, res) => {
   }
 };
 
-const updateTaskById = (req, res) => {
-  return res.json({ message: "updating task by ID" });
+const updateTaskById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const task = await Task.findByPk(id);
+
+    if (!task) {
+      return res.status(404).json({ message: "Task not found" });
+    }
+
+    const passedInfo = req.body;
+    if (!passedInfo) {
+      return res.status(500).json({ message: "Unable to update task" });
+    }
+
+    await Task.update(passedInfo, { where: { id } });
+
+    return res.status(200).json({ message: "Task updated" });
+  } catch (error) {
+    console.error(`ERROR | ${error.message}`);
+    return res.status(500).json(error);
+  }
 };
 
 const deleteTaskById = (req, res) => {
