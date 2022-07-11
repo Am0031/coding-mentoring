929d321d901df050acf4d24cb054a1f25a2b5a88
Amelie Pira <amelie.pira@gmail.com>
2022-07-11 09:56:21 +0100

    added getById and createTask functions

--- src/controllers/api/tasks.js
+++ src/controllers/api/tasks.js
@@ -56,12 +56,75 @@ const getTasks = async (req, res) => {
   }
 };
 
-const getTaskById = (req, res) => {
-  return res.json({ message: "getting task by ID" });
+const getTaskById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const temptask = await Task.findByPk(id, {
+      attributes: ["id", "taskName", "taskDescription", "taskLevel", "points"],
+      include: Framework,
+    });
+    const task = temptask.map((i) => i.dataValues);
+
+    if (!task) {
+      return res.status(500).json({ message: "Task not found" });
+    }
+
+    const formatTask = (each) => {
+      const id = each.id;
+      const taskName = each.taskName;
+      const taskDescription = each.taskDescription;
+      const taskLevel = each.taskLevel;
+      const points = each.points;
+      const frameworkId = each.framework.id;
+      const frameworkName = each.framework.frameworkName;
+
+      const response = {
+        id,
+        taskName,
+        taskDescription,
+        taskLevel,
+        points,
+        frameworkId,
+        frameworkName,
+      };
+      return response;
+    };
+
+    const formattedTask = tasks.map(formatTask);
+
+    console.log(formattedTask);
+    return res.json(formattedTask);
+
+    // return formattedTasks;
+  } catch (error) {
+    console.error(`ERROR | ${error.message}`);
+    return res.status(500).json(error);
+  }
 };
 
-const createTask = (req, res) => {
-  return res.json({ message: "creating task" });
+const createTask = async (req, res) => {
+  try {
+    const { taskName, taskDescription, taskLevel, frameworkId } = req.body;
+
+    if (!taskName || !taskDescription || !taskLevel || !frameworkId) {
+      return res.status(400).json({ message: "Unable to create task" });
+    }
+
+    // we could add the userId to a task model, so mentors can filter on that and choose to view only their tasks
+    // const { id } = req.session.user;
+
+    const newTask = await Task.create({
+      taskName,
+      taskDescription,
+      taskLevel,
+      frameworkId,
+    });
+
+    return res.status(200).json({ message: "Task created", newTask: newTask });
+  } catch (error) {
+    console.error(`ERROR | ${error.message}`);
+    return res.status(500).json(error);
+  }
 };
 
 const updateTaskById = (req, res) => {
