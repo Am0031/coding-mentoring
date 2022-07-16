f88ba6c3500f55eefb0aaba95f260a6a1c64b58c
Amelie Pira <amelie.pira@gmail.com>
2022-07-16 13:30:59 +0100

    added date setup for taskdeadline

--- src/controllers/api/assignedTasks.js
+++ src/controllers/api/assignedTasks.js
@@ -1,8 +1,11 @@
 const { AssignedTask } = require("../../models");
+const moment = require("moment");
 
 const assignTask = async (req, res) => {
   try {
-    const { partnershipId, taskId, taskDeadline } = req.body;
+    const { partnershipId, taskId } = req.body;
+
+    const taskDeadline = moment().add(14, "days").format("YYYY-MM-DD");
 
     const assignedTask = await AssignedTask.create({
       taskId,
